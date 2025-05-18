d9c9abf3448ed02c16cd5725f92b12611c1ca114
Kousei <jiangwenjiebilibili@gmail.com>
2025-05-18 14:43:44 +0800

    init

---
+++ src/main/java/com/kousei/leetcode/editor/cn/$00004MedianOfTwoSortedArrays.java
@@ -0,0 +1,15 @@
+package com.kousei.leetcode.editor.cn;
+
+public class $00004MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
